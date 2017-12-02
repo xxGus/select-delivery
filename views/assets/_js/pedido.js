081477a57e44541d4fa09316ab3216ec8250eb8e
xxGus <gustavo_baptista@live.com>
2017-12-02 00:50:07 -0200

    desenvolvimento remoção de itens do pedido

--- views/assets/_js/pedido.js
+++ views/assets/_js/pedido.js
@@ -7,40 +7,98 @@ var addProduto = document.getElementById('add-produto');
 var resumoPizza = document.getElementById('resumo-pizza');
 var resumoProduto = document.getElementById('resumo-produto');
 
-addPizza.onclick = function (ev) {
-    if(selectPizza.value != 0){
+var comentario = document.getElementById('comentario');
+
+var remover = document.getElementById('remover');
+
+function adicionar(slct, table) {
+    if (slct.value != 0) {
+
+        var newId = Math.floor((Math.random() * 99999) + 1);
         var newRow = document.createElement('tr');
-        var btnRemover = document.createElement('button');
+        newRow.id = 'linha' + newId;
 
-        btnRemover.value = newRow.rowIndex;
-        btnRemover.textContent = 'remover';
+        var chk = document.createElement('input');
+        chk.type = 'checkbox';
+        chk.name = 'chk-remover';
+        chk.value = 'linha' + newId;
 
-        btnRemover.onclick = function (ev2) {
-            resumoPizza.deleteRow(btnRemover.value);
-        };
-
-        newRow.insertCell(0).innerHTML = selectPizza.options[selectPizza.selectedIndex].text;
-        newRow.insertCell(1).innerHTML = selectPizza.value;
-        newRow.insertCell(2).appendChild(btnRemover);
-        resumoPizza.appendChild(newRow);
-    }
-};
-
-addProduto.onclick = function (ev) {
-    if (selectProduto.value != ""){
-        var newRow = document.createElement('tr');
-        var btnRemover = document.createElement('button');
-
-        btnRemover.value = newRow.rowIndex;
-        btnRemover.textContent = 'remover';
-
-        btnRemover.onclick = function (ev2) {
-            resumoProduto.deleteRow(btnRemover.value);
-        };
-
-        newRow.insertCell(0).innerHTML = selectProduto.options[selectProduto.selectedIndex].text;
-        newRow.insertCell(1).innerHTML = selectProduto.value;
-        newRow.insertCell(2).appendChild(btnRemover);
-        resumoProduto.appendChild(newRow);
+        newRow.insertCell(0).innerHTML = slct.options[slct.selectedIndex].text;
+        newRow.insertCell(1).innerHTML = slct.value;
+        newRow.insertCell(2).appendChild(chk);
+        table.appendChild(newRow);
     }
 }
+
+
+remover.onclick = function () {
+    try {
+        var qntLinhas = resumoPizza.rows.length;
+
+        // obtendo os checkboxes:
+        var checkboxes = document.getElementsByName('chk-remover');
+        var linhasMarcadas = false;
+
+        console.log(checkboxes.length);
+
+        for (i = checkboxes.length - 1; i >= 0; i--)
+            if (checkboxes[i].checked) {
+
+                document.getElementById(checkboxes[i].value).remove();
+
+                linhasMarcadas = true;
+            }
+
+        // obtendo a quantidade de linhas restantes
+        qntLinhas = resumoPizza.rows.length;
+
+        // se o usuario nao marcou nenhuma linha
+        if (!linhasMarcadas && qntLinhas)
+            alert('Marque as linhas que deseja remover!');
+
+    } catch (erro) {
+        // tratando o erro se nao tiver mais linhas
+        //se quiser o alerta do erro: alert(erro);
+    }
+}
+
+//addProduto.onclick = adicionar(resumoProduto, selectProduto);
+
+// function adicionar() {
+//
+//     // var qntLinhas = tabela.rows.length;
+//
+//     // var novaLinha = tabela.insertRow(qntLinhas);
+//
+//     var novaLinha = document.createElement('tr');
+//
+//     var novoId = Math.floor((Math.random()*99999)+1);
+//
+//     novaLinha.id = 'linha'+novoId;
+//
+//     console.log(novaLinha);
+//
+//     // var coluna1 = novaLinha.insertCell(0);
+//     // var coluna2 = novaLinha.insertCell(1);
+//     // var coluna3 = novaLinha.insertCell(2);
+//
+//     coluna1.innerHTML = selectPizza.options[selectPizza.selectedIndex].text;
+//     coluna2.innerHTML = selectPizza.value;
+//
+//     var chk = document.createElement('input');
+//     chk.type  = 'checkbox';
+//     chk.id  = 'chk-remover';
+//     chk.value = 'linha'+novoId;
+//
+//     // coluna3.appendChild(chk);
+// };
+
+
+var tipoPagamento = document.getElementById('tipo-pagamento');
+var pagSelected = document.getElementById('tipo-pag-selected');
+
+tipoPagamento.onclick = function (ev) {
+    if (tipoPagamento.value != '') {
+        pagSelected.innerHTML = tipoPagamento.options[tipoPagamento.selectedIndex].text;
+    }
+};
